c0e904cfd13066dbc994076cf3313e506d04912c
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2024-07-02 08:43:25 +0500

    Updated files

---
+++ about.js
@@ -0,0 +1,9 @@
+// Go to  home/shop page btn
+let shop_btn  = document.querySelector(".shop")
+shop_btn.addEventListener('click' , ()=>{
+    window.location = "index.html"
+}) 
+let cart_btn  = document.querySelector(".cart")
+cart_btn.addEventListener('click' , ()=>{
+    window.location = "cart.html"
+}) 
---
+++ cart.js
@@ -0,0 +1,10 @@
+// Go to about page btn
+let about_btn  = document.querySelector(".about")
+about_btn.addEventListener('click' , ()=>{
+    window.location = "about.html"
+}) 
+// Go to Home/Shop page btn
+let shop_btn  = document.querySelector(".shop")
+shop_btn.addEventListener('click' , ()=>{
+    window.location = "index.html"
+}) 
